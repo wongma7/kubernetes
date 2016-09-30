ad3cc8d8d47dccce68111259d24d0ab69e744a4f
Matthew Wong <mawong@redhat.com>
2016-09-30 17:50:00 -0400

    Fix groupName for certificates & rbac, set genclient=true for type PetSet

--- pkg/apis/certificates/v1alpha1/doc.go
+++ pkg/apis/certificates/v1alpha1/doc.go
@@ -14,8 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +groupName=certificates.k8s.io
 // +k8s:deepcopy-gen=package,register
 // +k8s:conversion-gen=k8s.io/kubernetes/pkg/apis/certificates
 
+// +groupName=certificates.k8s.io
 package v1alpha1 // import "k8s.io/kubernetes/pkg/apis/certificates/v1alpha1"
