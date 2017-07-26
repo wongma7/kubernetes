6cdba8ded863aaa4fd22bf0da0aa103a1918c819
Hemant Kumar <gethemant@gmail.com>
2017-07-26 15:12:35 -0400

    Implement controller and volume plugin changes for resizing
    Fix unit test
    Fix code to use new import paths

--- pkg/api/validation/validation_test.go
+++ pkg/api/validation/validation_test.go
@@ -8877,7 +8877,7 @@ func TestValidatePersistentVolumeClaimStatusUpdate(t *testing.T) {
 	}, api.PersistentVolumeClaimStatus{
 		Phase: api.ClaimPending,
 		Conditions: []api.PvcCondition{
-			api.PvcCondition{Type: api.PvcResizeStarted, Status: api.ConditionTrue},
+			{Type: api.PvcResizeStarted, Status: api.ConditionTrue},
 		},
 	})
 	scenarios := map[string]struct {
